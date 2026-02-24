45548ae312818a2bfd61ab29c723c27020b44647
Meek Meek <haggins.jameka@gmail.com>
2026-02-24 14:00:18 -0500

    I always do this where I don't pay attention to typos and then wonder why this isn't working. With the task.status it's 'completed' not complete, this is why I kept having vscode yell at me. Took me too long to realize this. But to be fair I am trying to fight an ear infection right now.

--- src/components/TaskItem/TaskItem.tsx
+++ src/components/TaskItem/TaskItem.tsx
@@ -5,3 +5,24 @@
 //shows the task status and priority
 // allow user to change task status
 //all user to delete task
+
+import type { TaskItemProps, TaskStatus } from "../../types";
+
+export function TaskItem({ task, onStatusChange, onDelete}: TaskItemProps) {
+  return (
+    <div style={{
+      border: "1px soild #ccc",
+      padding: "12px",
+      marginBottom: "8px",
+      background: 
+        task.status === "completed" ? "#e6ffe6" : "white"
+    }}>
+      <h3>{task.title}</h3>
+      <p>{task.description}</p>
+      <p>Status: {task.status}</p>
+      <p>Priority: {task.priority}</p>
+      <button onClick={() => onStatusChange(task.id, "completed")}>Mark Complete</button>
+      <button onClick={() => onDelete(task.id)}>Delete</button>
+    </div>
+  );
+}
